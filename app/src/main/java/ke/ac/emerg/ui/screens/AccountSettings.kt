78c358f71bfec12ae83305ad970aa4658870c4fe
star <juniorprogrammer09@gmail.com>
2023-11-05 04:05:43 +0300

    new screens

---
+++ app/src/main/java/ke/ac/emerg/ui/screens/AccountSettings.kt
@@ -0,0 +1,9 @@
+package ke.ac.emerg.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AccountSettings(navController: NavController) {
+
+}
---
+++ app/src/main/java/ke/ac/emerg/ui/screens/Activities.kt
@@ -0,0 +1,9 @@
+package ke.ac.emerg.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ActivitiesView(navController: NavController) {
+
+}
