ac2cd39c4c8ec2eb7e6ef529fe04317d8737cd34
kidiga <juniorprogrammer09@gmail.com>
2025-02-19 13:38:53 +0300

    update library versions and remove unwanted extra dependencies.

--- app/src/main/java/ke/ac/emerg/MainActivity.kt
+++ app/src/main/java/ke/ac/emerg/MainActivity.kt
@@ -30,12 +30,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContent {
 
             val consultationViewModel   = viewModel<ConsultationViewModel>()
-
             App {
                 Navigator(consultationViewModel)
             }
